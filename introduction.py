7f121c0b50b39e60b1bc8fea26da557e412caef3
Nego-03 <abedmumo03@gmail.com>
2025-02-03 16:25:49 +0300

    Initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display an output
+
+print("Abed")
+print("eMobilis")
+print(100)
+print(10+20)
---
+++ variable.py
@@ -0,0 +1,20 @@
+#Variable is a container used to store a value
+
+location = "westlands"
+firstname = "Abed"
+lastname ="Mumo"
+school ="eMobilis"
+
+x =39
+y =2
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname, lastname)
+print("My fullname is",firstname, lastname)
+print(school, "is a coding school")
+print(x+y)
+print("The product of", x, "and", y, "is", x*y )
+
