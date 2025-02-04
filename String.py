62db9352fc52d24b5dd5cea0e009cc1f36b3aca7
himuselefu-ibraah <ibrahimchristopher004@gmail.com>
2025-02-04 16:17:27 +0300

    Inital commit

---
+++ String.py
@@ -0,0 +1,19 @@
+text = "Hello There"
+course = "MIT"
+
+print(text)
+print(course)
+
+# Accesing an element in a string
+print(text[0])
+print(text[1])
+
+#Size of a string
+print(len(text))
+
+#modifying a string
+print(text.upper())
+print(course.lower())
+
+#String Cancatenation
+print(text + " " + course)
---
+++ datatype.py
@@ -0,0 +1,29 @@
+number = 50 #Int
+num = 56.98 #Float
+Greeting = "Hello there" #Str
+hasFeathers = True #Boolean
+
+#Data Structures
+cars = ["Mercedes", "Toyota", "Ferrari"] #List
+fruits =("Apple", "Orange","Banana") #Tuple
+countries = {"England", "France", "Germany"} #set
+student = {
+    "firstname" : "Ann",
+    "course" : "MIT",
+    "age" : 19,
+    "nationality" : "Kenyan"
+}
+
+print(number)
+print(num)
+print(Greeting)
+print(hasFeathers)
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student["nationality"])
+
+#Typecasting - Converting one datatype to another
+print(float(number))
+print(int(num))
