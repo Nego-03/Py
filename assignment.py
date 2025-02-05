6dc6381aaa80f8ea4aaef9de75a3138bd7155a6a
Nego-03 <abedmumo03@gmail.com>
2025-02-05 16:26:17 +0300

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,14 @@
+# A program to check whether a year is a leap or not
+year = 2020
+if year % 4 ==0 :
+     print( year, "is a leap year")
+else :
+     print(year, "is not a leap year")
+
+#Program to check  whether a letter is a vowel or a consonant
+
+letter = "b"
+if (letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u"):
+    print(letter, "is a vowel")
+else:
+    print(letter, "is a consonant")
